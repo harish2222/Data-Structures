c09e6065fe5e4f943cc0addc7b2f1be8048b2f98
HARISHKALVA <harishkalva4126@gmail.com>
2022-01-26 20:04:34 +0530

    balanced brackets Co-authored-by: svvsaiteja <svvsaiteja@users.noreply.github.com>

---
+++ stacks/stacks.py
@@ -0,0 +1,35 @@
+class Stack():
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def get_stack(self):
+        return self.items
+    
+    def is_empty(self):
+        return self.items == []
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+if __name__ == '__main__':
+    print(Stack)
+    myStack = Stack()
+    myStack.push("A")
+    myStack.push("B")
+    print(myStack.get_stack())
+    myStack.push("C")
+    myStack.push("D")
+    print(myStack.get_stack())
+    myStack.pop()
+    print(myStack.get_stack())
+    print(myStack.is_empty())
+    
+    print(myStack.peek())
+    
