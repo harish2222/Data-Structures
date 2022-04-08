c27416090066c6fdbfc2d119401d373bd86ab3cc
HARISHKALVA <harishkalva4126@gmail.com>
2022-04-08 20:49:03 +0530

    Arrays concept

---
+++ Arrays/insertions_sets.py
@@ -0,0 +1,31 @@
+def intersect_method(first_arr, second_arr):
+    i = 0
+    j = 0
+    intersection = []
+
+    while i < len(first_arr) and j < len(second_arr):
+        if first_arr[i] == second_arr[j]:
+            if i == 0 or first_arr[i] != first_arr[i - 1]:
+                intersection.append(first_arr[i])
+            i += 1
+            j += 1
+        elif first_arr[i] < second_arr[j]:
+            i += 1
+        else:
+            j += 1
+    return intersection
+
+
+def union_method(first_arr, second_arr):
+    pass
+
+if __name__ == '__main__':
+    A = [2, 3, 3, 5, 7, 11]
+    B = [3, 3, 7, 15, 31]
+
+    print(set(A).intersection(B))
+    print(set(A).union(B))
+    c = intersect_method(A, B)
+    d = intersect_method(B, A)
+    print(c)
+    print(d)
--- linked_list/rotate.py
+++ linked_list/rotate.py
@@ -1,5 +1,4 @@
 from LinkedList import LinkedList
-
 if __name__ == '__main__':
     llist = LinkedList()
     llist.append(1)
