204304e61c478dc6093470aeaf1032e2385ac298
HARISHKALVA <harishkalva14@gmail.com>
2023-01-23 21:18:17 +0530

    changes

--- qrcodes_make.py
+++ qrcodes_make.py
@@ -1,5 +1,9 @@
+import tensorflow as tf
 import qrcode
 z = input("Input the url or anything that you want to access it with qrcode:\t")
 y = input("Enter the file_name:\t")
 x = qrcode.make(z)
 x.save(y+".jpg")
+a = tf.constant(234,tf.int64)
+print(tf.config.list_physical_devices())
+print(a)
